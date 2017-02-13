e1447cf94b0f64c4a94b8ea078b9587002054ede
Avinint <avinint@hotmail.com>
2017-02-13 01:33:22 +0100

    hibernate files

---
+++ src/bean/Utilisateur.java
@@ -0,0 +1,54 @@
+package bean;
+
+public class Utilisateur {
+
+	private int id;
+	private String prenom;
+	private String nom;
+	private String email;
+	private String telephone;
+	
+	public Utilisateur(){}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+}
+
